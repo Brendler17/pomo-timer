9cb66425c1c1081ca6cde947dedc8614cea42759
Gustavo Brendler <gmbrendler@inf.ufpel.edu.br>
2023-07-20 16:21:59 -0300

    created default theme

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,23 @@
+export const defaultTheme = {
+  white: '#FFF',
+
+  'gray-100': '#E1E1E6',
+  'gray-300': '#C4C4CC',
+  'gray-400': '#8D8D99',
+  'gray-500': '#7C7C8A',
+  'gray-600': '#323238',
+  'gray-700': '#29292E',
+  'gray-800': '#202024',
+  'gray-900': '#121214',
+
+  'orange-100': '#FFDEAD',
+  'orange-300': '#F4A460',
+  'orange-500': '#FFA500',
+  'orange-700': '#FF8C00',
+  'orange-900': '#FF4500',
+
+  'red-500': '#AB222E',
+  'red-700': '#7A1921',
+
+  'yellow-500': '#FBA94C',
+}
