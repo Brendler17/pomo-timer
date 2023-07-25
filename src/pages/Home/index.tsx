24d7fe85976a97c85ab209ac4d4c41af10716ef7
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2023-07-24 21:45:43 -0300

    fixed home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,39 @@
+import { Play } from 'phosphor-react';
+import {
+  Button, CountdownContainer, FormContainer, HomeContainer, Separator,
+} from './styles';
+
 export function Home() {
-  return <h1>Home</h1>;
+  return (
+    <HomeContainer>
+      <form action="submit">
+        <FormContainer>
+          <label htmlFor="task">
+            Vou trabalhar em
+            <input id="task" type="text" />
+          </label>
+
+          <label htmlFor="minutesAmount">
+            durante
+            <input id="minutesAmount" type="number" />
+          </label>
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <Button type="submit">
+          <Play size={24} />
+          Começar
+        </Button>
+      </form>
+    </HomeContainer>
+  );
 }
