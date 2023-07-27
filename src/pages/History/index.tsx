8deca514159d7ac3502969411aeb44b330aed9c0
Gustavo Brendler <gmbrendler@inf.ufpel.edu.br>
2023-07-27 20:09:30 -0300

    created history page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,59 @@
+import { HistoryContainer, HistoryList } from './styles';
+
 export function History() {
-  return <h1>History</h1>;
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Área</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>PomoTimer</td>
+              <td>Estudo</td>
+              <td>30 minutos</td>
+              <td>Há 2 horas</td>
+              <td>Completo</td>
+            </tr>
+            <tr>
+              <td>PomoTimer</td>
+              <td>Estudo</td>
+              <td>30 minutos</td>
+              <td>Há 2 horas</td>
+              <td>Completo</td>
+            </tr>
+            <tr>
+              <td>PomoTimer</td>
+              <td>Estudo</td>
+              <td>30 minutos</td>
+              <td>Há 2 horas</td>
+              <td>Completo</td>
+            </tr>
+            <tr>
+              <td>PomoTimer</td>
+              <td>Estudo</td>
+              <td>30 minutos</td>
+              <td>Há 2 horas</td>
+              <td>Completo</td>
+            </tr>
+            <tr>
+              <td>PomoTimer</td>
+              <td>Estudo</td>
+              <td>30 minutos</td>
+              <td>Há 2 horas</td>
+              <td>Completo</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  );
 }
