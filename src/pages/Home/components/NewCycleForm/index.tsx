e7c9416e1c3b4bf1c65ae9fd9a5d34e0e793716b
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2023-08-23 17:02:09 -0300

    created newCycleForm  component

---
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -0,0 +1,61 @@
+import { useContext } from 'react';
+import { CyclesContext } from '../..';
+import {
+  FormContainer, MinutesAmountInput, OcupationInput, TaskInput,
+} from './styles';
+import { useFormContext } from 'react-hook-form';
+
+export function NewCycleForm() {
+  const { activeCycle } = useContext(CyclesContext);
+  const { register } = useFormContext();
+
+  return (
+    <FormContainer>
+      <label htmlFor="ocupation">
+        Vou
+        <OcupationInput
+          id="ocupation"
+          type="text"
+          placeholder="tarefa"
+          list="ocupation-suggestions"
+          disabled={!!activeCycle}
+          {...register('ocupation')}
+        />
+        <datalist id="ocupation-suggestions">
+          <option value="estudar">Estudar</option>
+          <option value="trabalhar">Trabalhar</option>
+        </datalist>
+      </label>
+      <label htmlFor="task">
+        em
+        <TaskInput
+          id="task"
+          type="text"
+          placeholder="dê um nome para sua tarefa"
+          list="task-suggestions"
+          disabled={!!activeCycle}
+          {...register('task')}
+        />
+        <datalist id="task-suggestions">
+          <option value="projeto 1">Projeto 1</option>
+        </datalist>
+      </label>
+
+      <label htmlFor="minutesAmount">
+        durante
+        <MinutesAmountInput
+          id="minutesAmount"
+          type="number"
+          placeholder="00"
+          step={5}
+          min={1}
+          max={60}
+          disabled={!!activeCycle}
+          {...register('minutesAmount', { valueAsNumber: true })}
+        />
+      </label>
+
+      <span>minutos.</span>
+    </FormContainer>
+  );
+}
