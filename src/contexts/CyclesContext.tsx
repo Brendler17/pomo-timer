c4666ec8668fe069e3a201ccaac94882237ac65b
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2023-08-23 18:09:53 -0300

    created cycles context

---
+++ src/contexts/CyclesContext.tsx
@@ -0,0 +1,100 @@
+import { ReactNode, createContext, useState } from 'react';
+
+interface CreateCycleData {
+  task: string;
+  ocupation: string;
+  minutesAmount: number;
+}
+
+interface Cycle {
+  id: string;
+  ocupation: string;
+  task: string;
+  minutesAmount: number;
+  startDate: Date;
+  interruptDate?: Date;
+  finishedDate?: Date;
+}
+
+interface CyclesContextType {
+  cycles: Cycle[];
+  activeCycle: Cycle | undefined;
+  activeCycleId: string | undefined;
+  amountSecondsPassed: number;
+  markCurrentCycleAsFinished: () => void;
+  setSecondsPassed(seconds: number): void;
+  createNewCycle(data: CreateCycleData): void;
+  interruptCurrentCycle: () => void;
+}
+
+interface CyclesContextProviderProps {
+  children: ReactNode;
+}
+
+export const CyclesContext = createContext({} as CyclesContextType);
+
+export function CyclesContextProvider({ children }: CyclesContextProviderProps) {
+  const [cycles, setCycles] = useState<Cycle[]>([]);
+  const [activeCycleId, setActiveCycleId] = useState<string | undefined>(undefined);
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+  function markCurrentCycleAsFinished() {
+    setCycles((state) => state.map((cycle) => {
+      if (cycle.id === activeCycleId) {
+        return { ...cycle, finishedDate: new Date() };
+      }
+      return cycle;
+    }));
+  }
+
+  function setSecondsPassed(seconds: number) {
+    setAmountSecondsPassed(seconds);
+  }
+
+  function createNewCycle(data: CreateCycleData) {
+    const id = String(new Date().getTime());
+
+    const newCycle: Cycle = {
+      id,
+      ocupation: data.ocupation,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+      startDate: new Date(),
+    };
+
+    setCycles((state) => [...state, newCycle]);
+    setActiveCycleId(id);
+    setAmountSecondsPassed(0);
+
+    // reset();
+  }
+
+  function interruptCurrentCycle() {
+    setCycles((state) => state.map((cycle) => {
+      if (cycle.id === activeCycleId) {
+        return { ...cycle, interrupDate: new Date() };
+      }
+      return cycle;
+    }));
+    setActiveCycleId(undefined);
+  }
+
+  return (
+    <CyclesContext.Provider value={{
+      cycles,
+      activeCycle,
+      activeCycleId,
+      amountSecondsPassed,
+      markCurrentCycleAsFinished,
+      setSecondsPassed,
+      createNewCycle,
+      interruptCurrentCycle,
+    }}
+    >
+      {children}
+    </CyclesContext.Provider>
+
+  );
+}
