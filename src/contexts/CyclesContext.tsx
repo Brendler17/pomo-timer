f7fda5ba2cdfe42e0c966e512fa5c37b86cfbca9
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2023-08-29 18:01:35 -0300

    saving state in storage

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,6 +1,7 @@
 import {
-  ReactNode, createContext, useReducer, useState,
+  ReactNode, createContext, useEffect, useReducer, useState,
 } from 'react';
+import { differenceInSeconds } from 'date-fns';
 import { Cycle, cyclesReducer } from '../reducers/cycles/reducer';
 import {
   addNewCycleAction, interrupCurrentCycleAction, markCurrentCycleAsFinishedAction,
@@ -33,12 +34,29 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
   const [cyclesState, dispatch] = useReducer(cyclesReducer, {
     cycles: [],
     activeCycleId: null,
+  }, (initialState) => {
+    const storedStateAsJSON = localStorage.getItem('@pomotimer:cycles-state-1.0.0');
+    if (storedStateAsJSON) {
+      return JSON.parse(storedStateAsJSON);
+    }
+    return initialState;
   });
 
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
   const { cycles, activeCycleId } = cyclesState;
-
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+    if (activeCycle) {
+      return differenceInSeconds(new Date(), new Date(activeCycle.startDate));
+    }
+    return 0;
+  });
+
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cyclesState);
+
+    localStorage.setItem('@pomotimer:cycles-state-1.0.0', stateJSON);
+  }, [cyclesState]);
 
   function markCurrentCycleAsFinished() {
     dispatch(markCurrentCycleAsFinishedAction());
